e3b395f45fb5806ffa22b7da01b97c5543fb316f
Khoza1 <78310663+Khoza1@users.noreply.github.com>
2024-01-16 18:27:58 +0200

    about me

---
+++ dark-mode.js
@@ -0,0 +1,11 @@
+const body = document.body;
+
+function toggleDarkMode() {
+    body.classList.toggle('dark-mode');
+}
+
+// Adicione um botão ou um evento para ativar o modo escuro
+function toggleMenu() {
+    var navList = document.getElementById("nav-list");
+    navList.classList.toggle("active");
+}
